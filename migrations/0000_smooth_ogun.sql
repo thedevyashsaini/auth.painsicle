9050ba66da5779847a3ebc616c82408c31f95347
Devyash Saini <dysaini2004@gmail.com>
2024-12-17 23:15:15 +0530

    cloudflare virsion

--- migrations/0000_smooth_ogun.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-	`id` text PRIMARY KEY NOT NULL,
-	`email` text NOT NULL,
-	`name` text,
-	`password` integer,
-	`providers` text NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,50 @@
-import { db } from './db/index.js';
-import { usersTable, SelectUser } from './db/schema.js';
 import { authorizer } from "@openauthjs/openauth"
-import { MemoryStorage } from "@openauthjs/openauth/storage/memory"
-import { PasswordAdapter } from "@openauthjs/openauth/adapter/password"
-import { GithubAdapter } from "@openauthjs/openauth/adapter/github";
-import { PasswordUI } from "@openauthjs/openauth/ui/password"
-import { handle } from "hono/vercel"
-// import { serve } from "@hono/node-server"
+import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare"
+import {
+  type ExecutionContext,
+  type KVNamespace,
+} from "@cloudflare/workers-types"
 import { subjects } from "./subjects.js"
+import { PasswordAdapter } from "@openauthjs/openauth/adapter/password"
+import { PasswordUI } from "@openauthjs/openauth/ui/password"
 
-async function getUser(email: string): Promise<SelectUser> {
-  // Get user from database
-  // Return user ID
-  return { id: "1", email, name: "Test User", providers: JSON.stringify(["password"]), password: null }
+interface Env {
+  CloudflareAuthKV: KVNamespace
 }
 
-const app = authorizer({
-  subjects,
-  storage: MemoryStorage({
-    persist: "./persist.json",
-  }),
-  providers: {
-    password: PasswordAdapter(
-      PasswordUI({
-        sendCode: async (email, code) => {
-          console.log(email, code)
-        },
+async function getUser(email: string) {
+  // Get user from database
+  // Return user ID
+  return "123"
+}
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    return authorizer({
+      storage: CloudflareStorage({
+        namespace: env.CloudflareAuthKV,
       }),
-    ),
-    github: GithubAdapter({
-      clientID: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      scopes: ["user:email", "user:name"],
-    })
+      subjects,
+      providers: {
+        password: PasswordAdapter(
+          PasswordUI({
+            sendCode: async (email, code) => {
+              console.log(email, code)
+            },
+          }),
+        ),
+      },
+      success: async (ctx, value) => {
+        if (value.provider === "password") {
+          return ctx.subject("user", {
+            id: await getUser(value.email),
+            email: value.email,
+            name: "John Doe",
+            providers: "password",
+          })
+        }
+        throw new Error("Invalid provider")
+      },
+    }).fetch(request, env, ctx)
   },
-  success: async (ctx, value) => {
-    if (value.provider === "password") {
-      return ctx.subject("user", {
-        id: (await getUser(value.email)).id,
-        email: value.email,
-        name: "sasa",
-        providers: JSON.stringify(["password"]),
-      })
-    } else if (value.provider === "github") {
-      console.log(value.tokenset);
-      return ctx.subject("user", {
-        id: "1",
-        email: "test@gmail.com",
-        name: "Test User",
-        providers: JSON.stringify(["github"]),
-      });
-    }
-    throw new Error("Invalid provider")
-  },
-})
-
-console.log("Server started")
-
-export const handler = handle(app)
-// serve(app)
+}
